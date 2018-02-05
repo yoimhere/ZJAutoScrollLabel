0efdbe8f7b0102fc5a4a106ee7f42209de9605fa
yoimhere <yuzhijie1230@qq.com>
2018-02-05 10:32:41 +0800

    11

--- Example/ZJAutoScrollLabel/ViewController.swift
+++ Example/ZJAutoScrollLabel/ViewController.swift
@@ -21,7 +21,11 @@ class ViewController: UIViewController {
         noticeView.backgroundColor = UIColor.lightGray.withAlphaComponent(0.5);
         view.addSubview(noticeView)
         
-        noticeView.scrollLabel.setTexts(["关关雎鸠，在河之洲。窈窕淑女，君子好逑。", "参差荇菜，左右流之。窈窕淑女，寤寐求之。", "求之不得，寤寐思服。悠哉悠哉，辗转反侧。", "参差荇菜，左右采之。窈窕淑女，琴瑟友之。", "参差荇菜，左右芼之。窈窕淑女，钟鼓乐之。"])
+        noticeView.scrollLabel.setTexts(["关关雎鸠，在河之洲。窈窕淑女，君子好逑。",
+                                         "参差荇菜，左右流之。窈窕淑女，寤寐求之。",
+                                         "求之不得，寤寐思服。悠哉悠哉，辗转反侧。",
+                                         "参差荇菜，左右采之。窈窕淑女，琴瑟友之。",
+                                         "参差荇菜，左右芼之。窈窕淑女，钟鼓乐之。"])
         noticeView.scrollLabel.resume()
     }
 
